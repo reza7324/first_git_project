e9ebc3d5831342a66a6845d74c78970f16d82c33
Reza Goodarzi <reza.2002.goodarzi@gmail.com>
2023-11-25 20:15:46 +0330

    Reminder codes (first part)

---
+++ file1.php
@@ -0,0 +1,43 @@
+<?php
+####### Null coalesce #######
+
+// $value1 = null;
+// $value2 = null;
+// $value3 = "Hello world!";
+// $result = $value1 ?? $value2 ?? $value3 ?? "Default";
+// echo "Result :" . $result;
+
+//------------------------------------------------------
+
+####### Ternary operator #######
+
+// $age = 20;
+// echo $is_adult = ($age > 18) ? true : false;
+
+//-----------------------------------------------
+
+#######
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
